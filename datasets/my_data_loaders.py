2b6b839e78774a04529864b61a647f2a86aab1ef
John Lalor <john.lalor@nd.edu>
2021-05-24 10:28:39 -0400

    bug

--- datasets/my_data_loaders.py
+++ datasets/my_data_loaders.py
@@ -37,10 +37,10 @@ class my_MNIST(data.Dataset):
             target and transforms it.
     """
     urls = [
-        'http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz',
-        'http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz',
-        'http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz',
-        'http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz',
+        'https://ossci-datasets.s3.amazonaws.com/mnist/train-images-idx3-ubyte.gz',
+        'https://ossci-datasets.s3.amazonaws.com/mnist/train-labels-idx1-ubyte.gz',
+        'https://ossci-datasets.s3.amazonaws.com/mnist/t10k-images-idx3-ubyte.gz', 
+        'https://ossci-datasets.s3.amazonaws.com/mnist/t10k-labels-idx1-ubyte.gz',
     ]
     raw_folder = 'raw'
     processed_folder = 'processed'
